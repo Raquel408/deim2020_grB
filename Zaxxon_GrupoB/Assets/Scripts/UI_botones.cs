3a1ef62a602081b38cacdb82a35c973af5b259e5
Raquel408 <72872486+Raquel408@users.noreply.github.com>
2021-03-02 17:06:43 +0100

    Zaxxon
    UI

---
+++ Zaxxon_GrupoB/Assets/Scripts/UI_botones.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI_botones : MonoBehaviour
+{
+    public void Inicio()
+    {
+        SceneManager.LoadScene("zaxxon_scene1");
+    }
+
+    public void Highscore()
+    {
+        SceneManager.LoadScene("Highscore");
+    }
+
+     
+    public void  Opciones()
+    {
+        SceneManager.LoadScene("");
+    }
+}
